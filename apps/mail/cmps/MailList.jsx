7efa175696e32717f4136142f7eb35c4eb209107
Noam Saar <noamsaar11@gmail.com>
2023-12-13 15:02:39 +0200

    Add MailList.jsx

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,15 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "./MailPreview.jsx"
+const { Link } = ReactRouterDOM
+
+
+export function MailList({ mails, onRemoveMail }) {
+    return (
+        <section className="mail-list">
+            {mails.map((mail) =>
+                <article key={mail.id} className="mail-item">
+                    <MailPreview mail={mail}/>
+                </article>
+            )}
+        </section>
+    )
 }
