0791d5f58cfab4455bec12ed7dee95e3a8bd5c38
Spiivak <edenspiivak@gmail.com>
2023-12-16 22:02:30 +0200

    Fix Image

--- apps/note/cmps/NoteHeader.jsx
+++ apps/note/cmps/NoteHeader.jsx
@@ -1,19 +1,28 @@
-
 const { Link, NavLink } = ReactRouterDOM
 
 export function NoteHeader() {
-    return <header className="note-header flex justify-center align-center full">
-        <div className="left-side flex">
-        <button className="note-btn btn-bars"><i className="fa-solid fa-bars"></i></button>
-            <Link to="/mail">
-                <img src="assets/img/keep.png" alt="" />
-            </Link>
-        </div>
+	return (
+		<header className="note-header flex justify-center align-center full">
+			<div className="left-side flex">
+				<button className="note-btn btn-bars">
+					<i className="fa-solid fa-bars"></i>
+				</button>
+				<Link to="/note">
+					<img src="/assets/img/keep.png" alt="" />
+				</Link>
+			</div>
 
-        <div className="left">
-            <button className="note-btn btn-about"><i className="fa-regular fa-circle-question"></i></button>
-            <button className="note-btn btn-settings"><i className="fa-solid fa-gear"></i></button>
-            <button className="note-btn btn-user"><i className="fa-regular fa-circle-user"></i></button>
-        </div>
-    </header>
+			<div className="left">
+				<button className="note-btn btn-about">
+					<i className="fa-regular fa-circle-question"></i>
+				</button>
+				<button className="note-btn btn-settings">
+					<i className="fa-solid fa-gear"></i>
+				</button>
+				<button className="note-btn btn-user">
+					<i className="fa-regular fa-circle-user"></i>
+				</button>
+			</div>
+		</header>
+	)
 }
--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -15,14 +15,7 @@ export function AppHeader() {
         <div className="left">
             <button className="btn btn-about"><i className="fa-regular fa-circle-question"></i></button>
             <button className="btn btn-settings"><i className="fa-solid fa-gear"></i></button>
-            {/* <button><img src="" alt="" /></button> */}
             <button className="btn btn-user"><i className="fa-regular fa-circle-user"></i></button>
         </div>
-        {/* <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav> */}
     </header>
 }
