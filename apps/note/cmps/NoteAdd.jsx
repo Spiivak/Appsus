3d2dec6efb27be4b788ece50fab56da7a1974581
Spiivak <edenspiivak@gmail.com>
2023-12-17 03:34:49 +0200

    Add when clicking outside of the div it will close it

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -1,79 +1,94 @@
-const { useState } = React
-import { NoteAddType } from './NoteAddType.jsx'
+const { useState, useRef, useEffect } = React;
+import { NoteAddType } from './NoteAddType.jsx';
 
 export function NoteAdd({ addNote }) {
-  const [selectedType, setSelectedType] = useState(null)
-  const [showButtons, setShowButtons] = useState(true)
+  const [selectedType, setSelectedType] = useState(null);
+  const [showButtons, setShowButtons] = useState(true);
+  const containerRef = useRef(null);
 
   const buttonConfigurations = [
     { type: 'noteTxt', title: 'Add Text Note', display: <span>Add a note...</span> },
     { type: 'noteTodos', title: 'Add Todo Note', display: <i className='ri-checkbox-line'></i> },
     { type: 'noteVideo', title: 'Add Video Note', display: <i className='ri-video-add-line'></i> },
     { type: 'noteImg', title: 'Add Image Note', display: <i className='ri-image-add-line'></i> },
-  ]
+  ];
 
   const onTypeChange = (type) => {
-    setSelectedType(type)
-    setShowButtons(false)
-  }
+    setSelectedType(type);
+    setShowButtons(false);
+  };
 
-  const goBack = () => {
-    setSelectedType(null)
-    setShowButtons(true)
-  }
+  const closeComponent = () => {
+    setSelectedType(null);
+    setShowButtons(true);
+  };
+
+  const handleClick = (event) => {
+    event.stopPropagation();
+  };
+
+  const handleClickOutside = (event) => {
+    if (containerRef.current && !containerRef.current.contains(event.target)) {
+      closeComponent();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <section className="note-add-section">
-    <article className="note-add">
+      <article className="note-add" ref={containerRef} onClick={handleClick}>
+        {showButtons && (
+          <div className="add-type-btns-notetxt">
+            {buttonConfigurations
+              .filter((btn) => btn.type === 'noteTxt')
+              .map((btn, idx) => (
+                <button
+                  key={idx}
+                  className={`btn btn-txt-note type-btn ${
+                    selectedType === btn.type ? 'active' : ''
+                  }`}
+                  onClick={() => onTypeChange(btn.type)}
+                  title={btn.title}
+                >
+                  {btn.display}
+                </button>
+              ))}
+          </div>
+        )}
 
-      {showButtons && (
-        <div className="add-type-btns-notetxt">
-          {buttonConfigurations
-            .filter((btn) => btn.type === 'noteTxt')
-            .map((btn, idx) => (
-              <button
-                key={idx}
-                className={`btn btn-txt-note type-btn ${
-                  selectedType === btn.type ? 'active' : ''
-                }`}
-                onClick={() => onTypeChange(btn.type)}
-                title={btn.title}
-              >
-                {btn.display}
-              </button>
-            ))}
-        </div>
-      )}
+        {/* Render other types */}
+        {showButtons && (
+          <div className="add-type-btns">
+            {buttonConfigurations
+              .filter((btn) => btn.type !== 'noteTxt')
+              .map((btn, idx) => (
+                <button
+                  key={idx}
+                  className={`btn type-btn ${
+                    selectedType === btn.type ? 'active' : ''
+                  }`}
+                  onClick={() => onTypeChange(btn.type)}
+                  title={btn.title}
+                >
+                  {btn.display}
+                </button>
+              ))}
+          </div>
+        )}
 
-      {/* Render other types */}
-      {showButtons && (
-        <div className="add-type-btns">
-          {buttonConfigurations
-            .filter((btn) => btn.type !== 'noteTxt')
-            .map((btn, idx) => (
-              <button
-                key={idx}
-                className={`btn type-btn ${
-                  selectedType === btn.type ? 'active' : ''
-                }`}
-                onClick={() => onTypeChange(btn.type)}
-                title={btn.title}
-              >
-                {btn.display}
-              </button>
-            ))}
-        </div>
-      )}
-
-      {selectedType && (
-        <div className="controls-container">
-          <button className="note-btn" onClick={goBack}>
-            Back
-          </button>
-          <NoteAddType addNote={addNote} type={selectedType} />
-        </div>
-      )}
-    </article>
+        {selectedType && (
+          <div className="controls-container">
+            <NoteAddType addNote={addNote} type={selectedType} />
+          </div>
+        )}
+      </article>
     </section>
-  )
+  );
 }
