fe63201d7d8626cbbf56afaf2b44df3fcb8b7897
Spiivak <edenspiivak@gmail.com>
2023-12-17 03:35:09 +0200

    Fix syntax

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -1,45 +1,45 @@
-const { useState, useRef, useEffect } = React;
-import { NoteAddType } from './NoteAddType.jsx';
+const { useState, useRef, useEffect } = React
+import { NoteAddType } from './NoteAddType.jsx'
 
 export function NoteAdd({ addNote }) {
-  const [selectedType, setSelectedType] = useState(null);
-  const [showButtons, setShowButtons] = useState(true);
-  const containerRef = useRef(null);
+  const [selectedType, setSelectedType] = useState(null)
+  const [showButtons, setShowButtons] = useState(true)
+  const containerRef = useRef(null)
 
   const buttonConfigurations = [
     { type: 'noteTxt', title: 'Add Text Note', display: <span>Add a note...</span> },
     { type: 'noteTodos', title: 'Add Todo Note', display: <i className='ri-checkbox-line'></i> },
     { type: 'noteVideo', title: 'Add Video Note', display: <i className='ri-video-add-line'></i> },
     { type: 'noteImg', title: 'Add Image Note', display: <i className='ri-image-add-line'></i> },
-  ];
+  ]
 
   const onTypeChange = (type) => {
-    setSelectedType(type);
-    setShowButtons(false);
-  };
+    setSelectedType(type)
+    setShowButtons(false)
+  }
 
   const closeComponent = () => {
-    setSelectedType(null);
-    setShowButtons(true);
-  };
+    setSelectedType(null)
+    setShowButtons(true)
+  }
 
   const handleClick = (event) => {
-    event.stopPropagation();
-  };
+    event.stopPropagation()
+  }
 
   const handleClickOutside = (event) => {
     if (containerRef.current && !containerRef.current.contains(event.target)) {
-      closeComponent();
+      closeComponent()
     }
-  };
+  }
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener('click', handleClickOutside)
 
     return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
 
   return (
     <section className="note-add-section">
@@ -90,5 +90,5 @@ export function NoteAdd({ addNote }) {
         )}
       </article>
     </section>
-  );
+  )
 }
